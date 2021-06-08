4aec49d500e4d89a26a10f90b4073a4e4683c2ee
cjust689 <justizz89@gmail.com>
2021-06-07 23:22:58 -0400

    Add files via upload

---
+++ src/views/Store.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="white section store">
+        <div class="row container">
+            <div class="col s3">
+                <ul id="slide-out" class="black-text">
+                    <li><a class="black-text" href="#!">Product Filter</a></li>
+                    <li><a class="black-text" href="#!">Shirts</a></li>
+                    <li>
+                        <div class="divider yellow darken-2"></div>
+                    </li>
+                    <li><a class="subheader black-text">Subheader</a></li>
+                    <li><a class="waves-effect black-text" href="#!">Third Link With Waves</a></li>
+                </ul>
+            </div>
+            <div class="col s9">
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/shop/T-Shirt MockUp_Black.png">
+                            <span class="card-title">$35</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Unity Arms Tee <br> (Black)</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">Buy</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/shop/T-Shirt MockUp_Blue.png">
+                            <span class="card-title">$35</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Unity Arms Tee <br> (Blue)</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">Buy</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/shop/T-Shirt MockUp_Green.png">
+                            <span class="card-title">$35</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Unity Arms Tee <br> (Green).</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">Buy</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/shop/UNITY_PHOTO_BOOK4.png">
+                            <span class="card-title">25$</span>
+                        </div>
+                        <div class="card-content">
+                            <p>Coffee Table Book</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">Buy</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import M from '../js/materialize';
+M.AutoInit();
+
+
+/* eslint-disable */
+import { getRandom, randomImage } from '../js/functions';
+export default {
+    name: 'Store',
+    components: {
+        //HelloWorld
+    },
+    methods: {
+        getRandom,
+        randomImage,
+    },
+    mounted() {
+        M.AutoInit(); // That way, it is only initialized when the component is mounted
+    }
+}
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue' //add the helloworld component to this vue
+</script>
+<style>
+@import "../styles/materialize.min.css";
+@import "../styles/style.css";
+</style>
---
+++ src/views/Training.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="training">
+        <div class="white section">
+            <div class="row container">
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/evan/DSC00907.jpg">
+                            <span class="card-title">Safety</span>
+                        </div>
+                        <div class="card-action">
+                            <router-link class="" to="/Safety">Enter</router-link>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/12.jpg">
+                            <span class="card-title">Rifle</span>
+                        </div>
+                        <div class="card-action">
+                            <router-link class="" to="/Rifle">Enter</router-link>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/20.jpg">
+                            <span class="card-title">Pistol</span>
+                        </div>
+                        <div class="card-action">
+                            <router-link class="" to="/Pistol">Enter</router-link>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/evan/DSC00903.jpg">
+                            <span class="card-title">First Aid</span>
+                        </div>
+                        <div class="card-action">
+                            <router-link class="" to="/Medical">Enter</router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import M from '../js/materialize';
+M.AutoInit();
+
+
+/* eslint-disable */
+import { getRandom, randomImage } from '../js/functions';
+export default {
+    name: 'Home',
+    components: {
+        //HelloWorld
+    },
+    methods: {
+        getRandom,
+        randomImage,
+    },
+    mounted() {
+        M.AutoInit(); // That way, it is only initialized when the component is mounted
+    }
+}
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue' //add the helloworld component to this vue
+</script>
+<style>
+@import "../styles/materialize.min.css";
+@import "../styles/style.css";
+</style>
