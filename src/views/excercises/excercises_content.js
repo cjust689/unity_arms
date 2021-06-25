9592969eec27b28fb7ba2d97c95776710563a662
Personal <justizz89@gmail.com>
2021-06-25 12:49:14 -0400

    Resolved Exercise dynamic content
    Exercises now render based on the slug

--- src/views/excercises/excercises_content.js
+++ src/views/excercises/excercises_content.js
@@ -1,15 +1,31 @@
 export const excerciseModules = {
-    'pistol': [{
-        'title': 'title',
-        'excercise': 'excercise text',
+    'rifle': [{
+        'title': 'Rifle Transition Drill',
+        'excercise': 'Improve target acquisition from various positions',
         'img': 'img url/path',
         'components': 'components that should be loaded'
     },
     {
-        'title': 'title',
-        'excercise': 'excercise text',
+        'title': 'Rifle Bill Drill',
+        'excercise': 'Improve speed while maintaining accuracy',
         'img': 'img url/path',
-        'components': 'components that should be loaded'
-    }]
+        'components': 'components that should be loaded',
+        'type': 'rifle'
+    }],
+    'pistol': [{
+        'title': 'Transition Drill',
+        'excercise': 'Improve target acquisition from various positions',
+        'img': 'img url/path',
+        'components': 'components that should be loaded',
+        'type': 'pistol'
+    },
+    {
+        'title': 'Bill Drill',
+        'excercise': 'Improve speed while maintaining accuracy',
+        'img': 'img url/path',
+        'components': 'components that should be loaded',
+        'type': 'pistol'
+    }],
+    
 
 }
--- src/views/training/Excercises.vue
+++ src/views/training/Excercises.vue
@@ -3,14 +3,22 @@
     <div class="relatedTraining">
         <div class="white section">
             <div class="row">
-                <div class="col s12 l3" v-for="(item, index) in trainingModules"  :key="index">
+                <div class="col s12 l3" v-for="t in trainingModule" :key="t">
                     <div class="card">
                         <div class="card-content">
-                            <span class="card-title"> {{ item.title }}</span>
-                            <p>{{ item.excercise }}</p>
+                            <span class="card-title"> {{ t.title }}</span>
+                            <p>{{ t.excercise }}</p>
                         </div>
+                        <ul>
+    </ul>
                         <div class="card-action">
-                            <router-link class="" to="/Grip">Enter</router-link>
+                            <router-link class="" to="/Grip">Start</router-link>
+                            <!-- When Clicked vue should load a new Drill Vue: 
+                            -This vue contains col s12 wiith the description of the drill
+                            -Another col s12 with either a video or illustration of the drill
+                            -Form to enter the users performance of the drill
+                            -If necessary shot timer etc is present with a start top and so on.
+                             -->
                         </div>
                     </div>
                 </div>
@@ -19,40 +27,43 @@
     </div>
 </template>
 <script>
-
-    let trainingModules ='';
+//     const collectionOfThings = {
+//   a: ["a1", "a2", "a3"],
+//   b: ["b1", "b2"],
+//   c: [],
+// };
+    //when initialized this is empty, but I would expect it to not be when the vue is rendered due to the beforeMount() in the component options. What gives?
 /* eslint-disable */
+let trainingModules = null;
 let init = (params) => {
+    console.log('init');
+    console.log(trainingModules);
     trainingModules = excerciseModules[params.type];
-    return trainingModules
-    //in theory this should define the traininmodules to be loaded dynamically, it works as of now but it's a little hacky. If you reload the page it doesnt work, not sure why. If you navigate via nav it works as desired. Surely a better way without creating another level of components/vues
+    //return trainingModules
+    //in theory this should define the traininmodules to be loaded dynamically, it works as of now but it's a little hacky. If you reload the page it doesnt work, not sure why. If you navigate via nav it works as desired, but it loads the previous instance. Surely a better way without creating another level of components/vues
 }
-
-
-import { getRandom, randomImage } from '../../js/functions';
-import { excerciseModules } from '../excercises/excercises_content.js';
+import { excerciseModules } from '../excercises/excercises_content.js'; //placeholder for JSON
 export default {
     name: 'excercises',
     components: {
     },
-    props: {
-        excerciseModules
+    props: ["type"],
+  data() {
+    return {
+      trainingModule: [],
+    };
+  },
+  watch: {
+    type: {
+      handler(t) {
+        this.trainingModule = excerciseModules[t];
+      },
+      immediate: true,
     },
-    methods: {
-        getRandom,
-        randomImage,
-        init,
-
-    },
-    data() {
-        return {
-            trainingModules,
-        }
-
-    },
+  },
     mounted() {
         M.AutoInit(); // That way, it is only initialized when the component is mounted
-        init(this.$route.params);
+        console.log('mounted');
     }
 }
 // @ is an alias to /src
