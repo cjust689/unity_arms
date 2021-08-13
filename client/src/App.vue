ec8de5b60e0fafe098e15a9b17f14962ce4314c1
Random Hebard <rayhebard@gmail.com>
2021-08-12 20:13:39 -0400

    Refactore banner componet

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,11 +9,9 @@
 
 <template>
     <div id="app container">
-        <div class="col s12 yellow darken-2 banner center-align">
-            <!-- <h5 class="bannerText center-align">UPCOMING  EVENTS</h5> -->
-            <router-link class="bannerText center-align black-text pulsate" to="/events">UPCOMING EVENTS</router-link>
-        </div>
 
+        <UnityBanner/>
+        
         <UnityHeader />
         
         <transition name="fade" mode="out-in">
@@ -28,37 +26,25 @@
         </transition>
 
         <div class="divider yellow darken-2"></div>
+
         <PP />
-        <!-- <div class="parallax-container">
-            <div class="parallax">
-                <iframe class="embed-responsive-item" id="ytplayer" type="text/html"  width="1920" height="1080" src="https://www.youtube.com/embed/EQBWmtXlKcM?playlist=EQBWmtXlKcM&loop=1&autoplay=1&controls=1&showinfo=0&mute=1" frameborder="0"></iframe>
-            </div>
-        </div> -->
-
+        
         <UnityFooter />
-
-        <!-- <div class="grey darken-4 center-align valign-wrapper footerBanner">
-            <div class="row">
-                <div class="col s12">
-                    <h5 class="grey-text darken-4">{{ randomQuote() }}</h5>
-                </div>
-            </div>
-        </div>
-        <div class="">
-            <h6 class="black-text center-align yellow darken-2">Copyright 2021</h6>
-        </div> -->
     </div>
 </template>
 <script>
 import { getRandom, randomQuote, randomImage } from './js/functions';
 import M from './js/materialize';
 const UnityHeader = () => import("./components/AppHeader");
-const UnityFooter = () => import("./components/Footer")
-const PP = () => import("./components/ParallaxProjector")
+const UnityFooter = () => import("./components/Footer");
+const PP = () => import("./components/ParallaxProjector");
+const UnityBanner = () => import("./components/Banner");
+
 export default {
 
     name: 'App',
     components:{
+        UnityBanner,
         UnityHeader,
         UnityFooter,
         PP
---
+++ client/src/components/Banner.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+       <div class="col s12 yellow darken-2 banner center-align">
+            <router-link class="bannerText center-align black-text pulsate" to="/events">UPCOMING EVENTS</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import M from '../js/materialize';
+export default {
+    name: 'Banner',
+    mounted() {
+        M.AutoInit();
+    }
+}
+</script>
